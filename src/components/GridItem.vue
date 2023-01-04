26c8ae0a914ff02ed351cee73c251c0f7cc13f3b
Elizabeth <eroberts22@my.whitworth.edu>
2023-01-03 22:15:25 -0800

    add hover on art portfolio

--- src/components/GridItem.vue
+++ src/components/GridItem.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="grid-item">
-    <img
-      :src="require('@/assets/' + image.src)"
-      :alt="format(image.title)"
-      class="grid-img"
-    />
-    <!-- <div class="overlay">
-      <div class="grid-item-description">Test</div>
-    </div> -->
-    <div class="grid-item-title">
+    <div class="grid-img-wrap">
+      <img
+        :src="require('@/assets/' + image.src)"
+        :alt="format(image.title)"
+        class="grid-img"
+      />
+      <div v-show="!mobile" class="grid-img-description-layer">
+        <div class="grid-img-description">
+          {{ image.title }}
+        </div>
+      </div>
+    </div>
+    <div v-show="mobile" class="grid-item-title">
       {{ image.title }}
     </div>
   </div>
@@ -20,15 +24,35 @@ export default {
   props: {
     image: Object,
   },
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.checkScreenForMobile);
+    this.checkScreenForMobile();
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.checkScreenForMobile);
+  },
   methods: {
     format(text) {
       return text.toLowerCase().trim().replace(" ", "-");
+    },
+    checkScreenForMobile() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 875) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
 .grid-item {
   color: var(--off-white);
   width: 100%;
@@ -38,60 +62,56 @@ export default {
   vertical-align: top;
   overflow: hidden;
   flex-direction: column;
+  font-family: "Poppins";
 }
 
 .grid-item-title {
   color: var(--off-white);
   padding-top: 5px;
   padding-bottom: 30px;
-  font-family: "Poppins";
+  font-size: calc(1em + 1vmin);
   cursor: default;
 }
-/* 
-.overlay {
-  position: absolute;
 
-  opacity: 0;
-  transition: .5s ease;
-  background-color: #008CBA;
-}
-
-.grid-item:hover .overlay {
-  opacity: 1;
-}
-
-.grid-item-description {
-  color: white;
-  font-size: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  text-align: center;
-} */
-
-/* .grid-item-description {
-  visibility: hidden;
-  opacity: 0;
-}
-
-.grid-item:hover .grid-item-description {
-  visibility: visible;
-  opacity: 1;
-} */
-
-.grid-item::after {
-  content: "";
-  display: block;
+.grid-img-wrap {
+  position: relative;
+  display: inline-block;
 }
 
 .grid-img {
   flex-shrink: 0;
   -webkit-flex-shrink: 0;
+  position: relative;
   max-width: 100%;
   max-height: 100%;
   padding: 20px;
+}
+
+.grid-img-description-layer {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: var(--transparent-black);
+  color: #fff;
+  visibility: hidden;
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: opacity 0.5s, visibility 0.5s;
+}
+
+.grid-img-wrap:hover .grid-img-description-layer {
+  visibility: visible;
+  opacity: 1;
+}
+
+.grid-img-description {
+  transition: 0.5s;
+  font-size: calc(1.2em + 1vmin);
+  text-align: center;
 }
 </style>
